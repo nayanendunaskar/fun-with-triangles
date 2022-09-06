79f01fc308d2a9c2eb5cdad5e23e718bf2acb7eb
nayanendu naskar <nayan19981@gmail.com>
2022-09-06 12:37:27 +0530

    js part done

--- quiz.js
+++ quiz.js
@@ -1,3 +1,22 @@
 const quizForm = document.querySelector(".quiz-form");
-const submitAnswerbtn = document.querySelector("#submit-answe-btn");
+const submitAnswerbtn = document.querySelector("#submit-answer-btn");
 const outputEl = document.querySelector("#output");
+const correctAnswers = ["90°", "right angled"];
+
+function calculateScore() {
+let score = 0;
+let index = 0;
+
+const formResults = new FormData(quizForm);
+for(let value of formResults.values()){
+    if(value === correctAnswers[index]){
+        score= score + 1;
+    }
+    index = index + 1;
+}
+
+outputEl.innerText = "Your score is " + score;
+
+}
+
+submitAnswerbtn.addEventListener("click", calculateScore);
