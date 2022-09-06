95886651a4e32ced44903d255a3194b371c57b80
nayanendu naskar <nayan19981@gmail.com>
2022-09-06 11:44:27 +0530

    line 5 updated

--- triangle.js
+++ triangle.js
@@ -1,3 +1,14 @@
 const inputs = document.querySelectorAll(".angle-input");
-const triangleBtn = document.querySelector("#triagle-btn");
+const triangleBtn = document.querySelector("#triangle-btn");
 const outputEl = document.querySelector("#output");
+
+function calculateSumOfAngles(angle1, angle2, angle3) {
+const sumOfAngles = angle1 + angle2 + angle3;
+return sumOfAngles;
+}
+
+function isItTriaangle() {
+
+}
+
+triangleBtn.addEventListener("click", isItTriaangle);
